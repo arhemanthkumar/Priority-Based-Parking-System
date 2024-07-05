7939bd904e082298b6797df6344f204adcb08985
arhemanthkumar <hemanthar95@gmail.com>
2024-07-06 00:56:58 +0530

    Removing unnecessary initial checking files.

--- number_plate_text_detector.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-from pytesseract import pytesseract
-import os
-# os.system('cd "/home/hemanth/Desktop/yolov4_old"')
-os.chdir('/home/hemanth/Desktop/yolov4_old/darknet')
-# print(os.curdir)
-print(os.getcwd())
-os.system('./darknet detector test data/obj.data cfg/yolov4-custom.cfg ../training/yolov4-custom_best.weights /home/hemanth/Desktop/number_plate_images/TC_00099.JPG -thresh 0.6')
